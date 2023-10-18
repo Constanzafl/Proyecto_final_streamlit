c055947111eb103f9b6eeceb4719eb5746b82f47
Constanzafl <florioconstanza@gmail.com>
2023-10-18 19:11:41 -0300

    o

--- pages/01_🍟_Restaurant.py
+++ pages/01_🍟_Restaurant.py
@@ -6,13 +6,15 @@ df_content = pd.read_csv('df_content.csv')
 
 # Definir una función para obtener recomendaciones
 def get_recommendations(restaurant_name):
-    # Encuentra la fila donde el nombre del restaurante coincida con la entrada
     restaurant_row = df_content[df_content['Restaurant_Name'] == restaurant_name]
 
-    # Obtén las recomendaciones del restaurante
-    recommendations = restaurant_row['Recommendations_content'].to_list()
-    
-    return recommendations
+    # Check if the restaurant was found
+    if not restaurant_row.empty:
+        recommendations = restaurant_row['Recommendations_content'].iloc[0]
+        recommendations_df = pd.DataFrame({'Recommendations': recommendations})
+        return recommendations_df
+    else:
+        return pd.DataFrame()
 
 restaurants = df_content.Restaurant_Name
 # Interfaz de usuario de Streamlit
@@ -29,6 +31,5 @@ if st.button("Obtener Recomendaciones"):
             st.write(f"No se encontraron recomendaciones para {restaurant_name}")
     else:
         st.write("Por favor, ingresa el nombre del restaurante.")
-
 
 
