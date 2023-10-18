2222adab4ffeec4ec7546ea92aff9c8387c2617f
Constanzafl <florioconstanza@gmail.com>
2023-10-18 19:23:44 -0300

    i

--- pages/01_🍟_Restaurant.py
+++ pages/01_🍟_Restaurant.py
@@ -6,15 +6,15 @@ df_content = pd.read_csv('df_content.csv')
 
 # Definir una función para obtener recomendaciones
 def get_recommendations(restaurant_name):
+    # Encuentra la fila donde el nombre del restaurante coincida con la entrada
     restaurant_row = df_content[df_content['Restaurant_Name'] == restaurant_name]
 
-    # Check if the restaurant was found
-    if not restaurant_row.empty:
-        recommendations = restaurant_row['Recommendations_content'].iloc[0]
-        recommendations_df = pd.DataFrame({'Recommendations': recommendations})
-        return recommendations_df
-    else:
-        return pd.DataFrame()
+    # Obtén las recomendaciones del restaurante
+    #recommendations = restaurant_row['Recommendations_content'].to_list()
+    recommendations1 = restaurant_row['Recommendations_content'].iloc[0]
+    recommendations = pd.DataFrame({'Recommendations': recommendations1})
+    
+    return recommendations
 
 restaurants = df_content.Restaurant_Name
 # Interfaz de usuario de Streamlit
@@ -22,11 +22,11 @@ st.title("Recomendaciones de Restaurantes")
 restaurant_name = st.selectbox("Selecciona un restaurante:", restaurants)
 if st.button("Obtener Recomendaciones"):
     if restaurant_name:
-        recommendations_df = get_recommendations(restaurant_name)
-        if recommendations_df:
+        recommendations = get_recommendations(restaurant_name)
+        if recommendations:
             st.write(f"Recomendaciones para {restaurant_name}:")
-            for recommendation_df in recommendations_df:
-                st.write(recommendation_df)
+            for recommendations in recommendations:
+                st.write(recommendations)
         else:
             st.write(f"No se encontraron recomendaciones para {restaurant_name}")
     else:
