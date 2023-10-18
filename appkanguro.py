8039a8626cba418dbec44b0949215671800aaec9
Constanzafl <florioconstanza@gmail.com>
2023-10-18 14:35:41 -0300

    o

--- appkanguro.py
+++ appkanguro.py
@@ -15,26 +15,32 @@ if st.checkbox('**Descubre la experiencia Kangaroo**'):
     st.write('Si viajaste a Florida y no sabes donde ir a comer, Kangaroo tiene la solución para vos')
 
 
-openai.api_key = st.secrets['OPENAI_API_KEY']
+#openai.api_key = st.secrets['OPENAI_API_KEY']
 
 st.title("Kanguro GPT!🤖")
 
-#dataset = pd.read_csv(r"C:\Users\flori\Desktop\Proyecto_final_streamlit\mini.csv") 
+dataset = pd.read_csv(r"C:\Users\flori\Desktop\Proyecto_final_streamlit\mini.csv") 
+
+
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Agrega el contenido del DataFrame al historial de conversación solo una vez
+if not st.session_state.get("data_added", False):
+    dataset_message = dataset.to_string(index=False) #f"Este es el contenido del DataFrame:\n{}"
+    st.session_state.messages.append({"role": "assistant", "content": dataset_message})
+    st.session_state.data_added = True
 
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
 # initialize model
 if "model" not in st.session_state:
     st.session_state.model = "gpt-3.5-turbo"
 
+
 # user input
 if user_prompt := st.chat_input("Tu consulta"):
+    # Agregar el mensaje del usuario
     st.session_state.messages.append({"role": "user", "content": user_prompt})
     with st.chat_message("user"):
         st.markdown(user_prompt)
@@ -43,11 +49,7 @@ if user_prompt := st.chat_input("Tu consulta"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        
-        # Agregar el contenido del conjunto de datos como mensaje
-        #st.session_state.messages.append({"role": "assistant", "content":dataset.to_string(index=False)})
-            
-    
+
         for response in openai.ChatCompletion.create(
             model=st.session_state.model,
             messages=[
@@ -57,10 +59,28 @@ if user_prompt := st.chat_input("Tu consulta"):
             stream=True,
         ):
             full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
+            if "role" not in response.choices[0].delta or response.choices[0].delta["role"] != "assistant":
+                # No mostrar el mensaje del asistente si no tiene un rol o si el rol no es "assistant"
+                message_placeholder.markdown(full_response + "▌")
 
-        message_placeholder.markdown(full_response)
+        if "role" in response.choices[0].delta and response.choices[0].delta["role"] == "assistant":
+            # Mostrar el mensaje del asistente al final
+            message_placeholder.markdown(full_response)
 
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
