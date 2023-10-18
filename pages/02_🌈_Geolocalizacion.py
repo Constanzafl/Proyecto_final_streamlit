efa3b3d88f493b41b3b5aa0aef867ad1a63ac9de
Constanzafl <florioconstanza@gmail.com>
2023-10-18 20:51:30 -0300

    i

--- pages/02_🌈_Geolocalizacion.py
+++ pages/02_🌈_Geolocalizacion.py
@@ -7,20 +7,11 @@ import pydeck as pdk
 # Define el usuario agente para Geopy
 geolocator = Nominatim(user_agent="mi_app_geopy")
 
-# Carga los datos del parquet y el parquet con los restaurantes mejor puntuados (?)
-#ruta = "FINAL_FLORIDA.parquet"
-#df = pd.read_parquet(ruta)
-
 ruta2 = "df_only_ubication.csv"
 df = pd.read_csv(ruta2)
 
-#mejores_por_tendencia = "predicciontendencia.parquet"
-#df_tendencia = pd.read_parquet(mejores_por_tendencia)
-
-#df_tendencia_unico = df_tendencia.drop_duplicates(subset="business_name", keep='first')
-
-mejores_por_tendencia = "predicciontendencia2.parquet"
-df_tendencia = pd.read_parquet(mejores_por_tendencia)
+mejores_por_tendencia = "predicciontendencia.csv"
+df_tendencia = pd.read_csv(mejores_por_tendencia)
 df_tendencia_unico = df_tendencia
 
 # Asegurémonos de que las columnas sean números
@@ -57,41 +48,25 @@ if st.button('Geocodificar'):
             # Ordenar por distancia y seleccionar los 5 más cercanos
             restaurantes_cercanos = restaurantes_df.sort_values(by='Distancia').head(5)
 
-            # Mostrar los 5 restaurantes más cercanos
-            st.write('Los 5 restaurantes más cercanos a tu ubicación:')
-            st.dataframe(restaurantes_cercanos[['business_name', 'Distancia']])
-
-
             # Lo mismo, pero con el de mejor tendencia
             df_tendencia_unico['Distancia'] = df_tendencia_unico.apply(lambda row: geodesic((lat_usuario, lon_usuario), (row['latitude'], row['longitude'])).meters, axis=1)
 
             # Ordenar por distancia y seleccionar los 5 más cercanos
-            tendencia_unico = df_tendencia_unico.sort_values(by='Distancia').head(2)
-
-            # Mostrar los 5 restaurantes más cercanos
-            st.write('El restaurante Tendencia mas cercano, es:')
-            st.dataframe(tendencia_unico[['business_name', 'Distancia', "longitude", "latitude"]])
-
+            tendencia_unico = df_tendencia_unico.sort_values(by='Distancia').head(1)
 
             usuario_df = pd.DataFrame({
                 "latitude": [lat_usuario],
-                "longitude": [lon_usuario]
+                "longitude": [lon_usuario],
+                "business_name":["Mi Ubicacion"]
             })
 
-            st.dataframe(df_tendencia_unico)
-
-            st.dataframe(restaurantes_cercanos)
-
-            st.write(df_tendencia_unico.dtypes)
-            st.write(restaurantes_cercanos.dtypes)
-
             # Crear el mapa
-            st.title("Mapa Personalizado")
+            st.title("Ubicacion de los Restaurantes")
 
             view_state = pdk.ViewState(
                 latitude=lat_usuario,
                 longitude=lon_usuario,
-                zoom=10,
+                zoom=11,
             )
 
             # Capa user
@@ -103,6 +78,7 @@ if st.button('Geocodificar'):
                 get_fill_color=[0, 0, 255], 
                 pickable=True,
                 auto_highlight=True,
+    
             )
 
             # Capa para los restaurantes
@@ -113,45 +89,90 @@ if st.button('Geocodificar'):
                 get_radius=300,
                 get_fill_color=[0, 255, 0],  
                 pickable=True,
-                auto_highlight=True,
+                auto_highlight=True, 
             )
 
 
+            #Carga el rest tendencia unico
+            layer_restaurante_unico = pdk.Layer(
+                type="ScatterplotLayer",
+                data=tendencia_unico,
+                get_position=["longitude", "latitude"],
+                get_radius=300,
+                get_fill_color=[255, 0, 0],  
+                pickable=True,
+                auto_highlight=True,
+            )
 
-            data = pd.DataFrame({
-                "latitude": [40.7228, 40.7129, 40.7130, 40.6931],
-                "longitude": [-74.0060, -74.0061, -74.0062, -74.0163],
-                "location_name": ["Ubicación 1", "Ubicación 2", "Ubicación 3", "Ubicación 4"],
-            })
-
-
-            # Capa el rest tendencia unico
-            # layer_restaurante_unico = pdk.Layer(
-            #     type="ScatterplotLayer",
-            #     data=tendencia_unico,
-            #     get_position=["longitude", "latitude"],
-            #     get_radius=300,
-            #     get_fill_color=[0, 255, 0],  
-            #     pickable=True,
-            #     auto_highlight=True,
-            # )
-
+            tooltip = {
+                "html": "Restaurant: <b>{business_name}</b>",
+                "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+            }
             # Mostrar el mapa
 
             # Crear el mapa PyDeck principal y agregar la capa de restaurantes
             mapa_pydeck = pdk.Deck(
-                layers=[layer_restaurantes, layer_user], #layer_restaurante_unico],
-                initial_view_state=view_state
+                layers=[layer_restaurantes, layer_user, layer_restaurante_unico],
+                initial_view_state=view_state,
+                tooltip=tooltip,
+                    views=[
+                        pdk.View(type="MapView", controller=True, legend="Restaurantes Cercanos")
+                    ]
             )
 
             # Mostrar el mapa
             st.pydeck_chart(mapa_pydeck)
+
+            def mostrar_leyenda():
+                st.markdown("#### Leyenda")
+                st.write(":large_blue_circle: Tu ubicación")
+                st.write(":large_green_circle: Restaurantes cercanos")
+                st.write(":red_circle: Restaurante recomendado")
+
+            # Llama a la función para mostrar la leyenda
+            mostrar_leyenda()
+
+
+            lat_usuario = usuario_df["latitude"].values[0]
+            lon_usuario = usuario_df["longitude"].values[0]
+
+            # Función para calcular la distancia y generar el enlace de Google Maps
+            def calculate_distance_and_generate_link(row):
+                lat_restaurante = row['latitude']
+                lon_restaurante = row['longitude']
+                enlace = f"https://www.google.com/maps/dir/{lat_usuario},{lon_usuario}/{lat_restaurante},{lon_restaurante}"
+                return enlace
+
+            restaurantes_cercanos['Enlace a Google Maps'] = restaurantes_cercanos.apply(calculate_distance_and_generate_link, axis=1)
+
+            enlaces_html = restaurantes_cercanos['Enlace a Google Maps'].apply(lambda enlace: f'<a target="_blank" href="{enlace}">Ver en Google Maps</a>')
+
+            restaurantes_cercanos['Enlace HTML'] = enlaces_html
+
+            restaurantes_cercanos_googlemaps = restaurantes_cercanos.loc[:,["business_name", "Distancia", "Enlace HTML"]]
+            restaurantes_cercanos_googlemaps = restaurantes_cercanos_googlemaps.rename(columns={'business_name': 'Nombre Restaurant', 'Distancia':'Distancia en Metros','Enlace HTML':'Indicaciones'})
+
+            st.header("Restaurantes Cercanos")
+            st.write(restaurantes_cercanos_googlemaps.to_html(escape=False, index=False), unsafe_allow_html=True)
+
+            #codigo para rest unico
+            tendencia_unico['Enlace a Google Maps'] = tendencia_unico.apply(calculate_distance_and_generate_link, axis=1)
+
+            enlaces_html_unico = tendencia_unico['Enlace a Google Maps'].apply(lambda enlace: f'<a target="_blank" href="{enlace}">Ver en Google Maps</a>')
+
+            tendencia_unico['Enlace HTML'] = enlaces_html_unico
+
+            df_tendencia_unico_googlemaps = tendencia_unico.loc[:,["business_name", "Distancia", "Enlace HTML"]]
+            df_tendencia_unico_googlemaps = df_tendencia_unico_googlemaps.rename(columns={'business_name': 'Nombre Restaurant', 'Distancia':'Distancia en Metros','Enlace HTML':'Indicaciones'})
+
+            st.header("")
+            st.header("")
+            st.header("Restaurantes Único")
+            st.write("(tendencia positiva en opiniones!)")
+            st.write(df_tendencia_unico_googlemaps.to_html(escape=False, index=False), unsafe_allow_html=True)
 
         else:
             st.error('No se encontraron coordenadas para la dirección ingresada.')
     else:
         st.warning('Ingrese una dirección válida.')
 
-
-
-#direccion de prueba: 935 W Okeechobee Rd, Hialeah, FL 33010, Estados Unidos
