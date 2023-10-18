b34197c04148a82bc4869ad57770663f4f3612b2
Constanzafl <florioconstanza@gmail.com>
2023-10-18 19:59:23 -0300

    i

--- pages/01_🍟_Restaurant.py
+++ pages/01_🍟_Restaurant.py
@@ -12,25 +12,22 @@ def get_recommendations(restaurant_name):
         recommendations = restaurant_row['Recommendations_content'].iloc[0]
         # Dividir las recomendaciones por líneas o cualquier otro separador
         recommendations_list = recommendations.split('\n')  # Cambia el separador según corresponda
-
-        # Crear una lista de diccionarios
-        recommendations_dict_list = [{'Recommendations': rec} for rec in recommendations_list]
-
-        recommendations_df = pd.DataFrame(recommendations_dict_list)
+        recommendations_df = pd.DataFrame({'Recommendations': recommendations_list})
         return recommendations_df
     else:
         return pd.DataFrame()
        
-restaurants = df_content.Restaurant_Name
+
 
 st.title('Recomendaciones de Restaurantes')
 
-
+restaurants = df_content.Restaurant_Name
 restaurant_name = st.selectbox("Selecciona un restaurante:", restaurants)
-recommendations_df1 = get_recommendations(restaurant_name)
 
-if not recommendations_df1.empty:
+recommendations_df = get_recommendations(restaurant_name)
+
+if not recommendations_df.empty:
     st.write('Recomendaciones para el restaurante:', restaurant_name)
-    st.write(recommendations_df1)
+    st.write(recommendations_df)
 else:
     st.write('No se encontraron recomendaciones para el restaurante especificado.')
