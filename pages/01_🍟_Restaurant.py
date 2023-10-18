057bc7427a6a3c3f5eb081d5a35498f3f5db9f60
Constanzafl <florioconstanza@gmail.com>
2023-10-18 19:29:00 -0300

    o

--- pages/01_🍟_Restaurant.py
+++ pages/01_🍟_Restaurant.py
@@ -4,32 +4,26 @@ import pandas as pd
 
 df_content = pd.read_csv('df_content.csv')
 
-# Definir una función para obtener recomendaciones
+# Definir una función para obtener las recomendaciones
 def get_recommendations(restaurant_name):
-    # Encuentra la fila donde el nombre del restaurante coincida con la entrada
     restaurant_row = df_content[df_content['Restaurant_Name'] == restaurant_name]
 
-    # Obtén las recomendaciones del restaurante
-    #recommendations = restaurant_row['Recommendations_content'].to_list()
-    recommendations1 = restaurant_row['Recommendations_content'].iloc[0]
-    recommendations = pd.DataFrame({'Recommendations': recommendations1})
-    
-    return recommendations
-
-restaurants = df_content.Restaurant_Name
-# Interfaz de usuario de Streamlit
-st.title("Recomendaciones de Restaurantes")
-restaurant_name = st.selectbox("Selecciona un restaurante:", restaurants)
-if st.button("Obtener Recomendaciones"):
-    if restaurant_name:
-        recommendations = get_recommendations(restaurant_name)
-        if recommendations:
-            st.write(f"Recomendaciones para {restaurant_name}:")
-            for recommendations in recommendations:
-                st.write(recommendations)
-        else:
-            st.write(f"No se encontraron recomendaciones para {restaurant_name}")
+    if not restaurant_row.empty:
+        recommendations = restaurant_row['Recommendations_content'].iloc[0]
+        recommendations_df = pd.DataFrame({'Recommendations': recommendations})
+        return recommendations_df
     else:
-        st.write("Por favor, ingresa el nombre del restaurante.")
+        return pd.DataFrame()
+restaurants = df_content.Restaurant_Name
+
+st.title('Recomendaciones de Restaurantes')
 
 
+restaurant_name = st.selectbox("Selecciona un restaurante:", restaurants)
+recommendations_df = get_recommendations(restaurant_name)
+
+if not recommendations_df.empty:
+    st.write('Recomendaciones para el restaurante:', restaurant_name)
+    st.write(recommendations_df)
+else:
+    st.write('No se encontraron recomendaciones para el restaurante especificado.')
