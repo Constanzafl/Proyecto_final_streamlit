a2541b6d847d6f62aa1b45a05d0ca2dedfbe517e
Constanzafl <florioconstanza@gmail.com>
2023-10-24 18:35:14 -0300

    o

--- utils/chatfuncions.py
+++ utils/chatfuncions.py
@@ -15,7 +15,8 @@ system = """ Eres un asistente especializado en recomendacion de restaurantes en
     - Responde sobre sitios que aparecen unicamente en el dataframe que se obtiene luego de colocar la direccion y filtrar por latitud y longitud los datos.
     Se le proporcionarán consultas sobre que sitios son mejores para comer cierto tipo de alimento. 
     - Si la comida que el usuario busca no se encuentra dentro de los datos proporcionados, responder que no se encuentra la informacion solicitada y brindar la opcion de preguntar sobre otro tipo de comida. 
-    - Si el usuario contesta Gracias, responde de nada. 
+    - Si la respuesta no esta en la información proporcionada, responde: "No tengo datos para responder su pregunta"
+    - Si el usuario pregunta por algo que no esta relacionado a restaurantes, responde: "soy un asistente para responder preguntas de restaurantes, no tengo la información solicitada en mi base de datos" 
     - No repitas dos veces la misma informacion, excepto que el usuario te vuelva a preguntar lo mismo. 
       
      """
